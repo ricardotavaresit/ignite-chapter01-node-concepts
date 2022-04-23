94e3e1409ec8894fb9c3c555066ed47560b0354b
rtavares <ricardotavaresit@gmail.com>
2022-04-23 17:18:56 +0100

    create route to delete tasks

--- src/index.js
+++ src/index.js
@@ -105,7 +105,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex( (todo) => todo.id === id );
+   
+  if( todoIndex === -1 ){
+    return response.status(404).json({error: `Id:${id} doesn't exists.`});
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
