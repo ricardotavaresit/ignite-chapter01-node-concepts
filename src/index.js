07c6abd201938f5dff886dfb8dea2e110c55e7bb
rtavares <ricardotavaresit@gmail.com>
2022-04-23 17:43:05 +0100

    general corrections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
 
@@ -46,10 +46,13 @@ app.post('/users', (request, response) => {
   return response.status(201).json(userData);
 });
 
+
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   return response.status(200).json(user.todos); 
 });
+
+
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
 
